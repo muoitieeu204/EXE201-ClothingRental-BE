086593ec5c202ab266c66b05b76b0c833e954939
hocpvse184940 <hocpvse184940@fpt.edu.vn>
2026-01-23 11:28:55 +0700

    [CREATE] add Wishlist service

---
+++ EXE201.service/Implementation/WishlistService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using EXE201.Repository.Implementations;
+using EXE201.Service.DTOs;
+using EXE201.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE201.Service.Implementation
+{
+    public class WishlistService : IWishlistService
+    {
+        private readonly UnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public WishlistService(UnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+
+        }
+        public async Task<WishlistDTO> createAsync(WishlistDTO entity)
+        {
+            var wishlist = _unitOfWork.Wishlists.AddAsync(entity);
+        }
+
+        public Task<bool> deleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<WishlistDTO>> getAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WishlistDTO> getByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<WishlistDTO> updateAsync(int id, WishlistDTO entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EXE201.service/Interface/IWishlistService.cs
@@ -0,0 +1,18 @@
+﻿using EXE201.Service.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE201.Service.Interface
+{
+    public interface IWishlistService
+    {
+        public Task<IEnumerable<WishlistDTO>> getAllAsync();
+        public Task<WishlistDTO> getByIdAsync(int id);
+        public Task<WishlistDTO> createAsync(WishlistDTO entity);
+        public Task<WishlistDTO> updateAsync(int id, WishlistDTO entity);
+        public Task<bool> deleteAsync(int id);
+    }
+}
